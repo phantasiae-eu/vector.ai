e5fc21b9e8783f753b9bac72d1dce3f33ce09fdf
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-08-11 00:06:54 +0100

    feat: cat boxes correctly shown

--- src/cats/cats.component.tsx
+++ src/cats/cats.component.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import catsData from '../data/cats'
 import axios from 'axios'
 import { key } from '../secretConfig'
@@ -7,8 +7,10 @@ import './styles/react-resizable.css'
 import GridLayout from 'react-grid-layout'
 import { Layout } from './cats.model'
 import { Cat } from '../data/cats.model'
+import { BallBeat } from 'react-pure-loaders'
 
 const Cats: React.FC = (): ReactElement => {
+    const [links, setLinks] = useState(catsData.map((): string => ''))
     useEffect((): void => {
         const fetchData = async (): Promise<string[]> => {
             const response = await axios(
@@ -16,11 +18,10 @@ const Cats: React.FC = (): ReactElement => {
             )
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             return response.data.data.map((data: any): string[] => {
-                console.log('data: ', data)
                 return data.embed_url
             })
         }
-        fetchData().then((data: string[]): void => console.log(data))
+        fetchData().then((data: string[]): void => setLinks(data))
     }, [])
     const layout: Layout[] = catsData.map(
         (cat: Cat, index: number): Layout => {
@@ -49,11 +50,14 @@ const Cats: React.FC = (): ReactElement => {
                         <div
                             onClick={(): void => console.log('mensola')}
                             style={{
-                                backgroundColor: 'red',
+                                backgroundColor: 'transparent',
                                 display: 'flex',
                                 flexDirection: 'column',
                                 justifyContent: 'center',
                                 alignItems: 'center',
+                                borderWidth: '1px',
+                                borderColor: 'grey',
+                                borderStyle: 'solid',
                             }}
                             key={index.toString()}
                         >
@@ -63,17 +67,23 @@ const Cats: React.FC = (): ReactElement => {
                                     position: 'absolute',
                                     color: 'white',
                                     top: '0',
+                                    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+                                    width: '100%',
                                 }}
                             >
-                                pippo
+                                {cat.title}
                             </p>
-                            <iframe
-                                src="https://giphy.com/embed/CjmvTCZf2U3p09Cn0h"
-                                width="100%"
-                                height="100%"
-                                frameBorder="0"
-                                sandbox={'allow-scripts allow-same-origin'}
-                            ></iframe>
+                            {links[index] ? (
+                                <iframe
+                                    src={links[index]}
+                                    width="100%"
+                                    height="100%"
+                                    frameBorder="0"
+                                    sandbox={'allow-scripts allow-same-origin'}
+                                ></iframe>
+                            ) : (
+                                <BallBeat color={'#123abc'} loading={true} />
+                            )}
                             <div
                                 style={{
                                     zIndex: 1,
@@ -82,7 +92,7 @@ const Cats: React.FC = (): ReactElement => {
                                     transform: 'translate(-50%, -50%)',
                                     width: '187px',
                                     height: '150px',
-                                    backgroundColor: 'rgba(0,0,0,0.5)',
+                                    backgroundColor: 'transparent',
                                     top: '75px',
                                 }}
                             ></div>
