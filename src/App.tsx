fd175952514eb31f0b9bf37a3e09f7d2c5e8d1f1
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-08-11 03:15:04 +0100

    refactor: style moved to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { ReactElement } from 'react'
 import './App.css'
 import Cats from '../src/cats/cats.component'
-import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
 
 const App: React.FC = (): ReactElement => {
     return (
